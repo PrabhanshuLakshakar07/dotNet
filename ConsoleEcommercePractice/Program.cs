b95cdfdd203412c29eefc0d5ce45d90eb5f5e7d3
PrabhanshuLakshakar07 <lakshakarprabhanshu@gmail.com>
2026-07-05 17:13:12 +0530

    console practice example

---
+++ ConsoleEcommercePractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleEcommercePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
